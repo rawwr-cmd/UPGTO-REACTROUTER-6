f5c65191c476ca3bff3de37fd1e970c69a040723
rawwr <akshayace0302@gmail.com>
2022-07-07 04:31:55 +0530

    upgrading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes, Navigate } from "react-router-dom";
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
+//redirect = navigate in the react router
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Switch>
-          <Route path='/welcome'>
-            <Welcome />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/welcome" />} />
+          <Route path="/welcome/*" element={<Welcome />}>
+            <Route path="new-user" element={<p>WELCOME, new user!</p>} />
           </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
       </main>
     </div>
   );
